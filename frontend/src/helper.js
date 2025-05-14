c21dd9782a5615877f38b4223a6d7f34f66e78d9
Shreyasdubeyy <shreyasdubey03@gmail.com>
2025-05-14 11:25:22 +0530

    latest

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -8,4 +8,4 @@ export const removeClass=(el,className)=>{
     elem.classList.remove(className)
 }
 
-export const api_base_url = "http://localhost:3000"
+export const api_base_url = import.meta.env.VITE_API_URL;
