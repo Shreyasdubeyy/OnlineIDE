bc0f1dd2c0d1ce6c9a1af6f46f05b7357072248d
Shreyasdubeyy <shreyasdubey03@gmail.com>
2025-10-13 01:58:36 +0530

    Changes in env  file

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -7,5 +7,10 @@ export const removeClass=(el,className)=>{
     let elem=document.querySelector(el)
     elem.classList.remove(className)
 }
-
-export const api_base_url = import.meta.env.VITE_API_URL;
+// Automatically pick backend depending on environment
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL          // local backend
+    : "https://onlineide-u37b.onrender.com";
+    
+export const api_base_url = API_URL;
